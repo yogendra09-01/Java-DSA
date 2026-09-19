d3c9b68b5d4f6514b1a0fc48c6a43222254d0293
yogendra09-01 <jogi9835@gmail.com>
2026-09-19 11:38:21 +0530

    Fix isPrime code

---
+++ Maths/binToDec.java
@@ -0,0 +1,19 @@
+public class binToDec {
+    public static void main(String[] args) {
+       decNum(101001); 
+    }
+
+    public static void decNum(int binNum){
+        int pow=0;
+        int decNum=0;
+
+        while(binNum>0){
+            int lastDigit= binNum % 10;
+            decNum= decNum +(lastDigit * (int)Math.pow(2,pow));
+            pow++;
+            binNum = binNum/10;
+
+        }
+        System.out.println(decNum);
+    }
+}
--- Maths/isPrime.java
+++ Maths/isPrime.java
@@ -1,15 +1,15 @@
 public class isPrime {
-    public static void main(String[] args) {
-	    System.out.println(isPrime(16)); 
+   public static void main(String[] args) {
+	    System.out.println(IsPrime(16)); 
 	}
-	static boolean isPrime(int n){
-	    boolean isPrime=true;
+	static boolean IsPrime(int n){
+	    boolean IsPrime=true;
 	    
 	    for(int i=2;i<=Math.sqrt(n);i++){
 	        if(n%i==0){
 	            return false;
 	        }
 	    }
-	    return isPrime;
+	    return IsPrime;
 	}
 }
